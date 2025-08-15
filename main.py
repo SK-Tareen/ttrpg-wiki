39d6f3bccf88742cb0ab4c3ba2585c49c1d7188a
Sania Khan Tareen <87571978+SK-Tareen@users.noreply.github.com>
2025-08-16 00:20:13 +0300

    modified:   main.py (#2)
    modified:   requirements.txt
    	modified:   utils/parse_pdf.py
        Conversion to PyMuPDF

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from utils.parse_pdf import parse_pdf
-from utils.chunking import chunk_book
-from utils.embedding import embed_and_store_faiss
-from utils.query import load_collection, query_book, ask_llm
+from utils.parse_pdf import parse_pdf, validate_pdf_file
 
 def main():
+    book_path="book.pdf"
     # Step 1: Parse PDF
-    data = parse_pdf("book.pdf", output_json_path="book.json")
-    if not data:
-        print("Parsing failed.")
+    is_valid, message = validate_pdf_file(book_path)
+    if is_valid:
+        result = parse_pdf(book_path, "book.json", max_workers=4)
+    else:
+        print(f"PDF validation failed: {message}")
         return
 
-    # Step 2: Chunk
-    chunks = chunk_book("book.json")
+    # # Step 2: Chunk
+    # chunks = chunk_book("book.json")
 
-    # Step 3: Embed + Store in Chroma
-    embed_and_store_faiss(chunks, persist_dir="chroma_db", batch_size=16)
+    # # Step 3: Embed + Store in Chroma
+    # embed_and_store_faiss(chunks, persist_dir="chroma_db", batch_size=16)
 
     # # Step 4: Query Loop
     # collection = load_collection("chroma_db")
